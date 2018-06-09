3d57b40eac26ffda895776fef841f597468d3a96
VuongBaNam <vuongbanam1995@gmail.com>
2018-06-09 13:26:05 +0700

    update feature

--- src/main/java/TestCase/Utils.java
+++ src/main/java/TestCase/Utils.java
@@ -1,7 +1,7 @@
 package TestCase;
 
 public class Utils {
-    public static final String IP_CONTROLLER = "127.0.0.1";
+    public static final String IP_CONTROLLER = "192.168.101.243";
     public static final int PORT = 5000;
     public static final double THRESHOLD_IAT = 0.2;
 }
